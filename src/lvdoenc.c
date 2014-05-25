16c404b710ae37cbdbb29a60befdd8d3ed6d2a49
Star Brilliant <m13253@hotmail.com>
2014-05-26 00:31:02 +0800

    Seperate lvdo_enc_frame

--- src/lvdoenc.c
+++ src/lvdoenc.c
@@ -5,8 +5,11 @@
 #include <string.h>
 #include "lvdocommon.h"
 
+static void lvdo_enc_frame(const unsigned char *payload, unsigned char *frame, unsigned int blocksize, unsigned int quantizer, unsigned int qmin, unsigned int qmax, unsigned int width, unsigned int height, double *in, double *out, const fftw_plan plan, const unsigned int *zigzag_reverse);
+
 int lvdo_dispatch(FILE *fi, FILE *fo, unsigned int blocksize, unsigned int quantizer, unsigned int qmin, unsigned int qmax, unsigned int width, unsigned int height, int grayonly) {
-    size_t payloadlen = (grayonly ? width*height : width*height*3/2)*(8-quantizer)*(qmax-qmin)/blocksize/blocksize/8;
+    size_t payloadleny = width*height*(qmax-qmin)*(8-quantizer)/blocksize/blocksize/8;
+    size_t payloadlen = grayonly ? payloadleny : payloadleny*3/2;
     unsigned char *payload = g_malloc(payloadlen);
     unsigned char *framey = g_malloc(width*height);
     unsigned char *frameuv = g_malloc(width*height/2);
@@ -19,45 +22,46 @@ int lvdo_dispatch(FILE *fi, FILE *fo, unsigned int blocksize, unsigned int quant
         memset(frameuv, 0x80, width*height/2);
     while(!feof(fi)) {
         int readres = fread(payload, 1, payloadlen, fi);
-        unsigned int payloadi = 0, blocki, blockj, pixeli, pixelj;
-        unsigned int lastbyte = 0, availbit = 0;
         if(readres == 0)
             break;
         if(readres < payloadlen)
             memset(payload+readres, 0, payloadlen-readres);
-        for(blocki = 0; blocki*blocksize < (grayonly ? height : height*3/2); blocki++)
-            for(blockj = 0; blockj*blocksize < width; blockj++) {
-                for(pixeli = qmin; pixeli < qmax; pixeli++) {
-                    if(availbit == 0) {
-                        lastbyte = payload[payloadi++];
-                        availbit = 8;
-                    } else if(availbit < 8-quantizer) {
-                        lastbyte |= ((unsigned int) payload[payloadi++])<<availbit;
-                        availbit += 8;
-                    }
-                    in[zigzag_reverse[pixeli]] = ((int) ((lastbyte&(0xff>>quantizer))<<quantizer)-128)/(blocksize*2.0);
-                    lastbyte >>= (8-quantizer);
-                    availbit -= 8-quantizer;
-                }
-                in[0] *= 2; // Normalize for FFTW
-                for(pixeli = 1; pixeli < blocksize; pixeli++)
-                    in[pixeli] *= M_SQRT2;
-                for(pixeli = blocksize; pixeli < blocksize*blocksize; pixeli += blocksize)
-                    in[pixeli] *= M_SQRT2;
-                fftw_execute(plan);
-                for(pixeli = 0; pixeli < blocksize; pixeli++)
-                    for(pixelj = 0; pixelj < blocksize; pixelj++)
-                        if(blocki*blocksize < height)
-                            framey[(blocki*blocksize+pixeli)*width+(blockj*blocksize+pixelj)] = prevent_char_overflow(round(out[pixeli*blocksize+pixelj]/blocksize)+128);
-                        else
-                            frameuv[(blocki*blocksize+pixeli-height)*width/2+(blockj*blocksize+pixelj)] = prevent_char_overflow(round(out[pixeli*blocksize+pixelj]/blocksize)+128);
-            }
+        lvdo_enc_frame(payload, framey, blocksize, quantizer, qmin, qmax, width, height, in, out, plan, zigzag_reverse);
         fwrite(framey, 1, width*height, fo);
+        if(!grayonly)
+            lvdo_enc_frame(payload+payloadleny, frameuv, blocksize, quantizer, qmin, qmax, width/2, height, in, out, plan, zigzag_reverse);
         fwrite(frameuv, 1, width*height/2, fo);
     }
-    fftw_free(in);
-    fftw_free(out);
-    fftw_destroy_plan(plan);
+    fftw_free(in); fftw_free(out); fftw_destroy_plan(plan);
+    g_free(payload); g_free(framey); g_free(frameuv); g_free(zigzag_reverse);
     return 0;
 }
 
+static void lvdo_enc_frame(const unsigned char *payload, unsigned char *frame, unsigned int blocksize, unsigned int quantizer, unsigned int qmin, unsigned int qmax, unsigned int width, unsigned int height, double *in, double *out, const fftw_plan plan, const unsigned int *zigzag_reverse) {
+    unsigned int payloadi = 0, blocki, blockj, pixeli, pixelj;
+    unsigned int lastbyte = 0, availbit = 0;
+    for(blocki = 0; blocki*blocksize < height; blocki++)
+        for(blockj = 0; blockj*blocksize < width; blockj++) {
+            for(pixeli = qmin; pixeli < qmax; pixeli++) {
+                if(availbit == 0) {
+                    lastbyte = payload[payloadi++];
+                    availbit = 8;
+                } else if(availbit < 8-quantizer) {
+                    lastbyte |= ((unsigned int) payload[payloadi++])<<availbit;
+                    availbit += 8;
+                }
+                in[zigzag_reverse[pixeli]] = ((int) ((lastbyte&(0xff>>quantizer))<<quantizer)-128)/(blocksize*2.0);
+                lastbyte >>= (8-quantizer);
+                availbit -= 8-quantizer;
+            }
+            in[0] *= 2; // Normalize for FFTW
+            for(pixeli = 1; pixeli < blocksize; pixeli++)
+                in[pixeli] *= M_SQRT2;
+            for(pixeli = blocksize; pixeli < blocksize*blocksize; pixeli += blocksize)
+                in[pixeli] *= M_SQRT2;
+            fftw_execute(plan);
+            for(pixeli = 0; pixeli < blocksize; pixeli++)
+                for(pixelj = 0; pixelj < blocksize; pixelj++)
+                    frame[(blocki*blocksize+pixeli)*width+(blockj*blocksize+pixelj)] = prevent_char_overflow(round(out[pixeli*blocksize+pixelj]/blocksize)+128);
+        }
+}
